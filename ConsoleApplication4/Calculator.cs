cd88016fcf288edb1ccb201bc66152bc3cd934d7
Adam <aashelt90@gmail.com>
2017-03-17 01:07:21 -0400

    Initial commit

---
+++ ConsoleApplication4/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        public int add(int x, int y)
+        {
+
+            return x + y;
+        }
+
+        public int subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int product(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int division(int x, int y)
+        {
+            return x / y;
+        }
+    }
---
+++ UnitTestTests/CalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        private Calculator calc = new Calculator();
+
+        [TestMethod()]
+        public void CalculatorTest()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void addTest()
+        {
+            Assert.AreEqual(2,calc.add(1, 1));
+        }
+
+        [TestMethod()]
+        public void subtractTest()
+        {
+            Assert.AreEqual(3, calc.subtract(6, 3));
+        }
+
+        [TestMethod()]
+        public void productTest()
+        {
+            Assert.AreEqual(5, calc.product(5, 1));
+        }
+
+        [TestMethod()]
+        public void divisionTest()
+        {
+            Assert.AreEqual(2, calc.division(4, 2));
+        }
+    }
+}
